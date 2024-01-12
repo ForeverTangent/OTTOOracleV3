f9b2556428eb42f92fcb3dd5b5ab2722083fd29e
Stan Rosenbaum <ForeverTangent@gmail.com>
2024-01-12 07:40:21 -0500

    Initial Commit

---
+++ OTTOOracleV3/OTTOOracleV3App.swift
@@ -0,0 +1,17 @@
+//
+//  OTTOOracleV3App.swift
+//  OTTOOracleV3
+//
+//  Created by Stanley Rosenbaum on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct OTTOOracleV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
